409564dd7ab5d3467c23d2d103c8787e3f4e02df
516445 <516445@LT103917.cts.com>
2019-10-18 19:33:30 +0530

    First Commit

---
+++ src/test/java/sample/demo.feature
@@ -0,0 +1,26 @@
+Feature: karate hello world example
+
+  Scenario: payment ID fetch
+    Given url 'http://onlinebookstorecognizant.us-east-2.elasticbeanstalk.com/addToCart'
+    * def json1 =
+      """
+      {
+       "user_id": 1,
+       "book_id": 1          
+      }
+      """
+    And request json1
+    When method POST
+    Then status 202
+    And match response.OrderID[*].paymentStatus contains ["NotStarted"]
+    * def partNumbers = karate.jsonPath(response,"$..paymentStatus")
+    * def payID = karate.jsonPath(response,"$..paymentID")
+    * print 'Payment Id:', payID[0]
+    * print 'The Request1 is ',json1
+    * print 'Response1 is: ',response
+    * print 'Response is: ',partNumbers
+    Given url 'http://onlinebookstorecognizant.us-east-2.elasticbeanstalk.com/buynow'
+    And request { "user_id": 1, "book_id": 1, "paymentid":'#(payID[0])'}
+    When method POST
+    Then status 202
+    * print 'The second response is: ' ,response
---
+++ src/test/java/sample/test.java
@@ -0,0 +1,36 @@
+package sample;
+
+import com.intuit.karate.KarateOptions;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+ 
+@KarateOptions(tags= {"~@ignore"},features = "classpath:sample/demo.feature")
+public class test {
+    @Test
+    public void testParallel() {
+        System.setProperty("karate.env", "demo"); // ensure reset if other tests (e.g. mock) had set env in CI
+        Results results = Runner.parallel(getClass(), 5);
+        generateReport(results.getReportDir());
+        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);        
+    }
+ 
+    public static void generateReport(String karateOutputPath) {        
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "demo");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();        
+    }
+ 
+} 
